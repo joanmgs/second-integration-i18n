e075bdbf326447ca74d367027424bd7e6c3d664d
Joan González <joan.gonzalez@192.168.1.191>
2023-04-11 23:28:07 -0500

    moved api files into its folders

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ProductsAPIResponse } from "../../types";
-import { defaultLocale } from "../../locale/constants";
-import { products } from "./db";
+import { ProductsAPIResponse } from "../../../types";
+import { defaultLocale } from "../../../locale/constants";
+import { products } from "../db";
 
 export default async function handler(
   req: NextApiRequest,
--- pages/api/tycs/index.ts
+++ pages/api/tycs/index.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { defaultLocale } from "../../locale/constants";
-import { TyCsAPIResponse } from "../../types";
-import { tycs } from "./db";
+import { defaultLocale } from "../../../locale/constants";
+import { TyCsAPIResponse } from "../../../types";
+import { tycs } from "../db";
 
 export default async function handler(
   req: NextApiRequest,
